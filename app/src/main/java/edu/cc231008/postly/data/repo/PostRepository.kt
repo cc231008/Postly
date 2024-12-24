d4ba4a05ab060b53366470dcc0c45146425d3535
cc231008 <cc231008@fhstp.ac.at>
2024-12-24 19:10:43 +0500

    DAO, Entity, Database, Repository

--- app/src/main/java/edu/cc231008/postly/data/repo/PostRepository.kt
+++ app/src/main/java/edu/cc231008/postly/data/repo/PostRepository.kt
@@ -1,7 +1,25 @@
 package edu.cc231008.postly.data.repo
 
 import edu.cc231008.postly.data.db.PostDAO
+import edu.cc231008.postly.data.db.PostEntity
+import kotlinx.coroutines.flow.map
 
 class PostRepository(private val postDao: PostDAO) {
+    val posts = postDao.getAllPosts()
+        .map { postList ->
+            postList.map { entity ->
+                PostTemplate(entity._id, entity.image, entity.description)
+            }
+        }
+
+    suspend fun findPostById(postId: Int): PostTemplate {
+        val userEntity = postDao.findPostById(postId)
+        return (PostTemplate(userEntity._id, userEntity.image, userEntity.description))
+    }
+
+    suspend fun addPost(image: String, description: String) {
+        postDao.addPost(PostEntity(0, image, description))
+    }
+
 
 }
