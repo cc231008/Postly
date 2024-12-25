9ac8a6e31229fef49e6c187cba80459bcbe80691
cc231008 <cc231008@fhstp.ac.at>
2024-12-25 17:05:22 +0500

    Commenting

--- app/src/main/java/edu/cc231008/postly/data/PostApplication.kt
+++ app/src/main/java/edu/cc231008/postly/data/PostApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import edu.cc231008.postly.data.db.PostDatabase
 import edu.cc231008.postly.data.repo.PostRepository
 
+//Ensures that repository is created only once.
 class PostApplication: Application() {
         val postRepository: PostRepository by lazy {
             val postDao = PostDatabase.getDatabase(this).postDao()
